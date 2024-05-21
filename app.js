6b2a0e8c6bc1808c6fed3ca4f623cc858245fee2
Orlando Stevens Villoslada Flores <computronica31@gmail.com>
2024-05-20 22:56:16 -0500

    agregando archivo js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var mensaje;
-var opcion = prompt("Introduzca su nombre:");
- 
-if (opcion == null || opcion == "") {
-        mensaje = "Has cancelado o introducido el nombre vacío";
-        } else {
-            mensaje = "Hola " + opcion;
-            }
-            document.write(mensaje);
+
+/* function solicitarnumero () {
+    const numero = prompt("ingreso un numero")
+    return Number(numero);
+}
+
+function sumar2 () {
+    const numero1 = solicitarnumero();
+    const numero2 = solicitarnumero();
+   
+
+return numero1 +   numero2;
+}
+
+const resultado2 = sumar2();
+console.log("Resultado: " + resultado2); */
+
+function suma() {
+    a = parseInt(prompt("ingrese 1er valor"));
+    b = parseInt(prompt("ingrese 2do valor"));
+    return (a + b);
+}
+
+const resultado = suma();
+console.log("el resultado es: " + resultado);
